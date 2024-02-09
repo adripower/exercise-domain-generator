8301e1cb20ff5d0e12007080e1ab56bff5f6545b
adripower <adriantorres.gc@gmail.com>
2024-02-09 11:46:05 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-let pronombre =["el","la","eso"];
-let adj =["grande","pequeño","alto"];
+
+let pronombre =["el","la","ese"];
+let adj =["misterio","especial","magico","espediente"];
+
 let noum =[".com",".es",".net"];
 
 for (let i = 0; i < pronombre.length; i++) {
-  
-    for (let a = 0; a < adj.length; a++) {
-  
-    for (let b = 0; b < noum.length; b++) {
-    
-        
-        console.log(pronombre[i] + adj[a] + noum[b]);
+      for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noum.length; b++) {
+                   console.log(pronombre[i] + adj[a] + noum[b]);
     }
 }
 
